d1f04e7d13ee8618392f5a52947193f6564d02f8
Green-Tea <sittingducks.lorenzo@gmail.com>
2023-12-08 01:32:50 +0700

    added router link to home when logo is pressed

--- src/App.vue
+++ src/App.vue
@@ -40,38 +40,40 @@ const menuitems = ref([
 </script>
 
 <template>
-<nav>
-  <div class="bg-gray-900 text-gray-100 px-3 py-5 md:flex justify-between item-center">
-    <!-- LOGO PART -->
-    <div class="flex text-red-500 text-l">
-      <RocketLaunchIcon class="h-6 w-6 text-blue-500 mr-2 md:mr-4 md:h-9 md:w-9" />
-      <h1 class="text-l md:text-2xl">TEXTBOOK BAY</h1>
-    </div>
-    <!-- MENU PART-->
-    <ul class="md:flex md:items-center">
-      <li class="md:mx-4" v-for="menuitem in menuitems" v-bind:key="menuitem.id">
-        <RouterLink :to="menuitem.link" class="hover:text-amber-400 md:text-xl">{{menuitem.name}}</RouterLink>
-      </li>
-    </ul>
-    <div>
-      <div v-if="user">
-        Welcome, {{ user.email }}
-        <button @click="logout">Logout</button>
+  <nav>
+    <div class="bg-gray-900 text-gray-100 px-3 py-5 md:flex justify-between item-center">
+      <!-- LOGO PART -->
+      <div class="flex text-red-500 text-l">
+        <RouterLink to="/" class="flex items-center">
+          <RocketLaunchIcon class="h-6 w-6 text-blue-500 mr-2 md:mr-4 md:h-9 md:w-9" />
+          <h1 class="text-l md:text-2xl">TEXTBOOK BAY</h1>
+        </RouterLink>
       </div>
-      <div v-else>
-        <RouterLink to="/login">Login</RouterLink>
-        <RouterLink to="/register">Register</RouterLink>
+
+      <!-- MENU PART-->
+      <ul class="md:flex md:items-center">
+        <li class="md:mx-4" v-for="menuitem in menuitems" v-bind:key="menuitem.id">
+          <RouterLink :to="menuitem.link" class="hover:text-amber-400 md:text-xl">{{menuitem.name}}</RouterLink>
+        </li>
+      </ul>
+      <div>
+        <div v-if="user">
+          Welcome, {{ user.email }}
+          <button @click="logout">Logout</button>
+        </div>
+        <div v-else>
+          <RouterLink to="/login">Login</RouterLink>
+          <RouterLink to="/register">Register</RouterLink>
+        </div>
       </div>
     </div>
-  </div>
-  <div v-if="user" @click="goToAddRecipe" class="fixed bottom-4 right-4 bg-blue-500 text-white p-4 rounded-full cursor-pointer">
-    <span>+</span> <!-- You can replace this with an icon -->
-  </div>
-</nav>
-
-<RouterView />
-
+    <div v-if="user" @click="goToAddRecipe" class="fixed bottom-4 right-4 bg-blue-500 text-white p-4 rounded-full cursor-pointer">
+      <span>+</span> <!-- You can replace this with an icon -->
+    </div>
+  </nav>
+  <RouterView />
 </template>
+
 
 <style>
 /* Add styles for the floating action button */
