8966293938eb050293fe22e843120875faea7f12
apivit45muic <apiwit.chn@student.mahidol.edu>
2023-12-08 19:47:12 +0700

    create and add footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="container mx-auto px-4 pb-10" ref="scrollContainer">
-    <h2 class="font-dm-serif text-4xl font-bold py-5 my-6">Hot Recipes</h2>
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      <RecipeCard v-for="recipe in hotRecipes" :key="recipe.id" :recipe="recipe" />
-    </div>
+  <div ref="scrollContainer">
+    <div class="container mx-auto px-4 pb-10">
+      <h2 class="font-dm-serif text-4xl font-bold py-5 my-6">Hot Recipes</h2>
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <RecipeCard v-for="recipe in hotRecipes" :key="recipe.id" :recipe="recipe" />
+      </div>
 
-    <h2 class="font-dm-serif text-4xl font-bold my-6 py-5 pt-10">New Recipes</h2>
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      <RecipeCard v-for="recipe in displayedRecipes" :key="recipe.id" :recipe="recipe" />
+      <h2 class="font-dm-serif text-4xl font-bold my-6 py-5 pt-10">New Recipes</h2>
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <RecipeCard v-for="recipe in displayedRecipes" :key="recipe.id" :recipe="recipe" />
+      </div>
+      <div v-if="isLoading && !allRecipesLoaded" class="text-center mt-4">
+        Loading more recipes...
+      </div>
     </div>
-    <div v-if="isLoading && !allRecipesLoaded" class="text-center mt-4">
-      Loading more recipes...
-    </div>
+    <Footer />
   </div>
 </template>
 
@@ -19,6 +22,7 @@
 import { ref, onMounted } from 'vue';
 import RecipeCard from '../components/RecipeCard.vue';
 import recipeService from '@/services/recipeService.js';
+import Footer from '@/components/Footer.vue';
 
 const allRecipes = ref([]);
 const displayedRecipes = ref([]);
