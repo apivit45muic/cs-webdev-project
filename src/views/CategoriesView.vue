18a77afbf484d7ee9fddc48cac3b897f4ad23ab5
apivit45muic <apiwit.chn@student.mahidol.edu>
2023-12-09 18:28:35 +0700

    minor ui changes

--- src/views/CategoriesView.vue
+++ src/views/CategoriesView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container mx-auto px-4 py-6">
-      <h1 class="text-2xl font-bold mb-4">Browse Recipes by Category</h1>
+      <h1 class="font-dm-serif text-2xl font-bold mb-4 py-6">Browse Recipes by Category</h1>
       <div class="flex flex-wrap gap-4">
         <category-tag v-for="category in categories" :key="category" :category="category" />
       </div>
